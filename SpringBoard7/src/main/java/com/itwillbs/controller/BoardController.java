0f32ad98a18c877785ea0f5b5b7eeaa7902bf970
ziwoneee <ziwonee24@gmail.com>
2025-06-11 13:02:57 +0900

    게시판 글쓰기 기능, 뷰페이지(/board/regist.jsp) 연결

--- SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // @RequestMapping(value = "/board/*")
 // => 실행하는 주소가 /board/~ 시작하는 모든 주소를
@@ -18,7 +19,18 @@ public class BoardController {
 
 	// 기능을 정의
 	
+	// http://localhost:8088/controller/board/regist (x)
+	// http://localhost:8088/board/regist (o)
 	
+	// 글쓰기 (정보 입력) / GET
+	@RequestMapping(value = "/regist", method = RequestMethod.GET)
+	public void boardRegistGET() {
+		logger.info(" boardRegistGET() 실행 ");
+		logger.info(" /views/board/regist.jsp 페이지 이동 ");
+	}
+	
+	// 글쓰기 (정보 처리) / POST
+
 	
 	
 	
---
+++ SpringBoard7/src/main/webapp/WEB-INF/views/board/regist.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!-- 템플릿 헤더 추가 -->
+<!-- ./ 상대주소	./WEB-INF/views <=>	/webapp/WEB-INF/views
+     ../ 절대주소	상위폴더로 이동
+      
+      -->
+      
+	<%@ include file = "../include/header.jsp" %>
+
+	<h1> /board/regist.jsp </h1>
+
+	<%@ include file = "../include/footer.jsp" %>
+<!-- 템플릿 푸터 추가 -->
