854f6c25db9fa26380f2bbfeabcc94c532ca08a4
ziwoneee <ziwonee24@gmail.com>
2025-06-11 11:56:56 +0900

    BoardController 생성 & 컨트롤러 매핑 완료

---
+++ SpringBoard7/src/main/java/com/itwillbs/controller/BoardController.java
@@ -0,0 +1,42 @@
+package com.itwillbs.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// @RequestMapping(value = "/board/*")
+// => 실행하는 주소가 /board/~ 시작하는 모든 주소를
+//    해당컨트롤러가 처리하겠다 
+
+@Controller
+@RequestMapping(value = "/board/*")
+public class BoardController {
+	
+	//mylog
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+	// 기능을 정의
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // BoardController
+
+
+
+
+
+
+
+
+
+
+
+
+
